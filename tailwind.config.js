fd3a763c88b37856765025d45a66f22b4336759b
NabilMustofaa <nabilmustofa6@gmail.com>
2023-10-23 22:22:58 +0700

    Cahnge using app router

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
-/** @type {import('tailwindcss/types').Config} */
-const config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       fontFamily: {
         poppins: ["Poppins", "sans"],
-      },
+      }, 
     },
   },
   plugins: [],
-};
-
-module.exports = config;
+}
