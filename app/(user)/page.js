a9b22cb49977a4aa00e296576ea389410192754c
Rakyan Krisna <rakyankrisna@gmail.com>
2023-11-05 08:56:25 +0700

    add view

---
+++ app/(user)/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <section className="header relative pt-16 items-center flex h-screen max-h-860-px">
+      
+    </section>
+    )
+}
