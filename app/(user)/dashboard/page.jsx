4e5e21a45c7c5d6aeb0620cc7cae9d96bd1bde97
Rakyan Krisna <rakyankrisna@gmail.com>
2024-01-02 09:01:15 +0700

    update dashboard page components

--- app/(user)/dashboard/page.jsx
+++ app/(user)/dashboard/page.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 import CardTable from '@/components/Cards/CardTable'
 import CardStats from '@/components/Cards/CardStats'
+import TrainingModal from '@/components/Popups/TrainingModal'
 
-export default function page() {
+export default function Page() {
+
+  const [isRecommendationModalOpen, setIsRecommendationModalOpen] = useState(false);
+
+
   return (
-    <div className='flex flex-col 2xl:w-[1440px] mx-auto mt-20 h-full'>
-      <div className="flex flex-row w-full justify-between">
+    <div className='flex flex-col '>
+      <TrainingModal isOpen={isRecommendationModalOpen} closeModal={() => setIsRecommendationModalOpen(false)} />
+      <div className="flex flex-row justify-between 2xl:w-[1440px] mx-auto mt-20 h-full">
         <div className="w-1/4 px-4">
         <CardStats />
         </div>
@@ -19,8 +26,8 @@ export default function page() {
         <CardStats />
         </div>
       </div>
-      <div className="h-2/3 px-4 py-8">
-        <CardTable />
+      <div className="h-2/3 px-4 py-8 2xl:w-[1440px] mx-auto">
+        <CardTable openModalTraining={() => setIsRecommendationModalOpen(true)} />
       </div>
     </div>
 
