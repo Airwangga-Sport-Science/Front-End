4586f96131e4c364228d32f8756435959d9ba099
Rakyan Krisna <rakyankrisna@gmail.com>
2024-03-21 16:08:49 +0700

    fix minor text Home Page and deletion layout.jsx

--- components/Navbars/IndexNavbar.jsx
+++ components/Navbars/IndexNavbar.jsx
@@ -17,7 +17,7 @@ export default function Navbar({ user, handleLogOut }) {
 					<div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
 						<Link href="/">
 							<p className={`text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap ${isActive("/")}`}>
-								Soccer
+								Home
 							</p>
 						</Link>
 						{user ? (
