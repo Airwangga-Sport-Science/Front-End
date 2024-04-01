37956bfb6cb019dfa39fec3c7c748526a18242e6
NabilMustofaa <nabilmustofa6@gmail.com>
2024-04-01 08:35:47 +0700

    update test

--- components/Forms/FormLanding.test.js
+++ components/Forms/FormLanding.test.js
@@ -9,14 +9,13 @@ describe('FormLanding Component', () => {
   });
 
   it('renders title correctly', () => {
-    const titleElement = screen.getByText('This is Landing Template', { selector: '#Title' });
+    const titleElement = screen.getByText('Attention!', { selector: '#Title' });
     expect(titleElement).toBeInTheDocument();
   });
 
   it('renders description correctly', () => {
     const descriptionElement = screen.getByText(
-      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt ipsam sequi maxime quae praesentium ipsa ab unde consectetur cum laborum minima vitae sint commodi non ipsum, magni aspernatur veritatis dolore?',
-      { selector: '#Description' }
+      /Mohon perhatian bahwa pengisian data pada formulir/i
     );
     expect(descriptionElement).toBeInTheDocument();
   });
