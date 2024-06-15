4d108547282f09b64a0ea5d5ebdafb38edc9637f
Rakyan Krisna <rakyankrisna@gmail.com>
2024-06-15 20:47:14 +0700

    update Static text on FormLanding.jsx

--- components/Forms/FormLanding.jsx
+++ components/Forms/FormLanding.jsx
@@ -9,9 +9,21 @@ export default function FormLanding() {
             Mohon perhatian bahwa pengisian data pada formulir berikut ini dimaksudkan untuk menggambarkan perbandingan komposisi kemampuan individu dan bukan sebagai nilai yang sesuai dengan standar global. Mohon isikan nilai sesuai dengan perbandingan komposisi kemampuan Anda dan sesuai dengan rentang yang Anda miliki. Sebagai contoh, jika Anda memiliki kemampuan sprint yang jauh lebih baik daripada heading, Anda dapat mengisikan nilai 90 untuk sprint dan nilai 50 untuk heading. Terima kasih atas perhatiannya.
           </p>
           <br></br>
-          <i>
-            Please be informed that filling out the data in the following form is intended to illustrate the comparison of abilities&apos; composition and does not represent values aligned with global standards. Kindly input values according to your own composition comparison and within the range you possess. For example, if you have a significantly better sprint ability compared to heading, you may input a value of 90 for sprint and 50 for heading. Thank you for your attention
-          </i>
+          <p align="center">Klik <a href="https://www.ea.com/en-gb/games/fifa/fifa-23/ratings" target="_blank"><u>tautan ini</u></a> untuk melihat contoh kumpulan data pemain dan kemampuannya!</p>
+          <br></br>
+          <br></br>
+          <br></br>
+          <p>
+            <i>
+              Please be informed that filling out the data in the following form is intended to illustrate the comparison of abilities&apos; composition and does not represent values aligned with global standards. Kindly input values according to your own composition comparison and within the range you possess. For example, if you have a significantly better sprint ability compared to heading, you may input a value of 90 for sprint and 50 for heading. Thank you for your attention
+            </i>
+          </p>
+          <br></br>
+          <p align="center">
+            <i>
+              Click on <a href="https://www.ea.com/en-gb/games/fifa/fifa-23/ratings" target="_blank"><u>this URL</u></a> to find out the example list of the player and their abilities!
+            </i>
+          </p>
         </div>
     </div>
   )
